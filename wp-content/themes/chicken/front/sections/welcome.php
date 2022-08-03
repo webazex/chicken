45acea41c62286832a4d214e18ddcb07d26404e6
webazex <webazex@gmail.com>
2022-08-03 20:29:50 +0300

    hell

--- wp-content/themes/chicken/front/sections/welcome.php
+++ wp-content/themes/chicken/front/sections/welcome.php
@@ -1,1 +1,81 @@
+<section>
+    <div class="site-size">
+        <div class="site-size__recipes-grid-container">
+            <div class="recipes-grid-container__new-receipes">
+                <img src="img/p1.png" class="new-receipes__img so" alt="">
+                <div class="new-receipes__content">
+                    <span class="content__title-box">Нові рецепти від Знатної Курки</span>
+                    <span class="content__desc-box">порадуй себе та здивуй рідних</span>
+                    <a href="" class="content__link-btn-box">
+                        <span class="link-btn-box__text">До розділу</span>
+                        <svg width="11" height="10" viewBox="0 0 11 10" fill="none" xmlns="http://www.w3.org/2000/svg" class="link-btn-box__icon">
+                            <path d="M0.676505 9.04036L9.65092 0.959757M9.65092 0.959757L3.57357 1.0493M9.65092 0.959757L8.92661 6.99446" stroke="white" stroke-width="1.4"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="recipes-grid-container__marked-receipes">
+                <img src="img/p2.png" class="marked-receipes__img so" alt="">
+                <div class="marked-receipes__content">
+                    <span class="content__title-box">Зустрічай! Мітболи з філе</span>
+                    <span class="content__desc-box">смачні та соковиті, на 100% знатні</span>
+                    <a href="" class="content__link-btn-box">
+                        <span class="link-btn-box__text">Детальніше</span>
+                        <svg width="11" height="10" viewBox="0 0 11 10" fill="none" xmlns="http://www.w3.org/2000/svg" class="link-btn-box__icon">
+                            <path d="M0.676505 9.04036L9.65092 0.959757M9.65092 0.959757L3.57357 1.0493M9.65092 0.959757L8.92661 6.99446" stroke="white" stroke-width="1.4"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="recipes-grid-container__slider-container">
+                <div class="slider-container__slider">
+                    <!--							==start=slide==-->
+                    <div class="slider__container-slide">
+                        <div class="container-slide__slide">
+                            <div class="slide__text-block">
+                                <div class="text-block__slide-title">
+                                    <span class="slide-title__text">Готуй смачніше ніж будь-коли</span>
+                                </div>
+                                <p class="slide__desc">Сотні пошагових рецептів від знатної курки</p>
+                                <a href="" class="text-block__link">
+                                    <span class="link__slide-text">Знатні рецепти</span>
+                                </a>
+                            </div>
+                            <img src="img/collage%20large.png" class="slide__img" alt="">
+                        </div>
+                    </div>
+                    <!--							==end=slide==-->
+                    <div class="slider__container-slide">
+                        <div class="container-slide__slide">
+                            <div class="slide__text-block">
+                                <div class="text-block__slide-title">
+                                    <span class="slide-title__text">Готуй смачніше ніж будь-коли</span>
+                                </div>
+                                <p class="slide__desc">Сотні пошагових рецептів від знатної курки</p>
+                                <a href="" class="text-block__link">
+                                    <span class="link__slide-text">Знатні рецепти</span>
+                                </a>
+                            </div>
+                            <img src="img/collage%20large.png" class="slide__img" alt="">
+                        </div>
+                    </div>
+                    <div class="slider__container-slide">
+                        <div class="container-slide__slide">
+                            <div class="slide__text-block">
+                                <div class="text-block__slide-title">
+                                    <span class="slide-title__text">Готуй смачніше ніж будь-коли</span>
+                                </div>
+                                <p class="slide__desc">Сотні пошагових рецептів від знатної курки</p>
+                                <a href="" class="text-block__link">
+                                    <span class="link__slide-text">Знатні рецепти</span>
+                                </a>
+                            </div>
+                            <img src="img/collage%20large.png" class="slide__img" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php
