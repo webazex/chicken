7f6b97e05e2371af4391fbae729f7b739bb3feb1
webazex <webazex@gmail.com>
2022-08-14 15:40:40 +0300

    dfsdf

---
+++ wp-content/themes/chicken/front/sections/about/quality.php
@@ -0,0 +1,11 @@
+<section class="control-section">
+    <div class="site-size">
+        <div class="site-size__control-section">
+            <img src="<?php echo $args['image']; ?>" alt="">
+            <div class="control-section__text-block">
+                <h2><?php echo $args['title'];?></h2>
+                <?php echo $args['content'];?>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ wp-content/themes/chicken/front/sections/about/welcome.php
@@ -0,0 +1,40 @@
+<section class="welcome-section">
+    <div class="site-size">
+        <div class="site-size__about-welcome-section">
+            <h2 class="about-welcome-section__title"><?php echo $args['welcome']['title'];?></h2>
+            <div class="about-welcome-section__content">
+                <p>
+                    <?php echo $args['welcome']['desc'];?>
+                </p>
+                <img src="<?php echo $args['welcome']['image'];?>" alt="">
+            </div>
+            <?php if(!empty($args['banner'])):?>
+            <div class="about-welcome-section__banner">
+                <?php if(!empty($args['banner']['content'])):?>
+                <p>
+                    <?php echo $args['banner']['content'];?>
+                </p>
+                <?php
+                endif;
+                if(!empty($args['banner']['image'])):
+                ?>
+                <img src="<?php echo $args['banner']['image']; ?>" class="banner__img" alt="">
+                <?php endif; ?>
+            </div>
+            <?php
+            endif;
+            if(!empty($args['advantages'])):
+            ?>
+            <div class="about-welcome-section__row-content">
+                <img src="<?php echo $args['advantages']['image'];?>" class="row-content__img" alt="">
+                <div class="row-content__text-box">
+                    <h3><?php echo $args['advantages']['title'];?></h3>
+                    <p>
+                        <?php echo $args['advantages']['content']; ?>
+                    </p>
+                </div>
+            </div>
+            <?php endif;?>
+        </div>
+    </div>
+</section>
