3cad5b875ee24fdfff0b898c3022deae8f6bda02
webazex <webazex@gmail.com>
2022-08-16 16:57:23 +0300

    fix homepage and add dump

--- wp-content/themes/chicken/front/sections/front-page/advantages-four-col.php
+++ wp-content/themes/chicken/front/sections/front-page/advantages-four-col.php
@@ -1,35 +1,22 @@
-<?php ?>
+<?php //print_r($args); ?>
 <section>
     <div class="site-size">
+        <?php if(!empty($args)):?>
         <div class="site-size__grid four-columns">
+            <?php foreach ($args as $k => $item):
+                if(!empty($item['icon'])){
+                    $icon = $item['icon'];
+                }else{
+                    $icon = get_template_directory_uri().'/img/0'.($k+1).'.png';
+                }
+            ?>
             <div class="four-columns__item-card">
-                <img src="img/01.png" class="item-card__icon">
-                <h4 class="item-card__title">Тільки природні корми</h4>
-                <p class="item-card__desc">
-                    спеціально розроблені,
-                    унікальні рецепти кормів з власновирощеного зерна</p>
+                <img src="<?php echo $icon; ?>" class="item-card__icon">
+                <h4 class="item-card__title"><?php echo $item['title'];?></h4>
+                <p class="item-card__desc"><?php echo $item['desc'];?></p>
             </div>
-            <div class="four-columns__item-card">
-                <img src="img/01.png" class="item-card__icon">
-                <h4 class="item-card__title">Тільки природні корми</h4>
-                <p class="item-card__desc">
-                    спеціально розроблені,
-                    унікальні рецепти кормів з власновирощеного зерна</p>
-            </div>
-            <div class="four-columns__item-card">
-                <img src="img/01.png" class="item-card__icon">
-                <h4 class="item-card__title">Тільки природні корми</h4>
-                <p class="item-card__desc">
-                    спеціально розроблені,
-                    унікальні рецепти кормів з власновирощеного зерна</p>
-            </div>
-            <div class="four-columns__item-card">
-                <img src="img/01.png" class="item-card__icon">
-                <h4 class="item-card__title">Тільки природні корми</h4>
-                <p class="item-card__desc">
-                    спеціально розроблені,
-                    унікальні рецепти кормів з власновирощеного зерна</p>
-            </div>
+            <?php endforeach;?>
         </div>
+        <?php endif;?>
     </div>
 </section>
