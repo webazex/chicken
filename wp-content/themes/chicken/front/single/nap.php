bd58fa53617de15bc5a0daff5b3bb66a20f7250e
webazex <webazex@gmail.com>
2022-08-20 21:41:29 +0300

    article

--- wp-content/themes/chicken/front/single/nap.php
+++ wp-content/themes/chicken/front/single/nap.php
@@ -1,5 +1,7 @@
 <?php
     get_header();
+    $content = $args['fields'];
+    $tags = $args['cats'];
     $recents = getRecentsNap();
 ?>
 <main>
@@ -14,14 +16,25 @@
                         get_template_part('front/components/breadcrumbs', '', $data);
                     ?>
                 </div>
-                <div class="topped-row__block-info"></div>
+                <div class="topped-row__block-info">
+                    <?php if(!empty($tags)):?>
+                    <div class="block-info__tags">
+                        <?php
+                            foreach ($tags as $tag):
+                                ?><span>#<?php echo $tag['name'];?></span><?php
+                            endforeach;
+                        ?>
+                    </div>
+                    <?php endif;?>
+                    <?php echo get_the_date( "d F Y", $post->ID ); //the_time("d F Y");?>
+                </div>
             </div>
-            <img src="<?php echo $args['image'];?>" alt="<?php echo $args['title'];?>" class="site-size-nap__image">
+            <img src="<?php echo $content['image'];?>" alt="<?php echo $content['title'];?>" class="site-size-nap__image">
             <h1 class="site-size-nap__title">
-                <?php echo $args['title'];?>
+                <?php echo $content['title'];?>
             </h1>
             <div class="site-size-nap__article">
-                <?php echo $args['content'];?>
+                <?php echo $content['content'];?>
             </div>
             <div class="site-size-nap__bottomed-row">
                 <div class="bottomed-row__socials">
@@ -29,6 +42,7 @@
                 </div>
                 <a href=""></a>
             </div>
+            <h2 class="recents-title"><?php echo $content['recents-title'];?></h2>
 
         </div>
     </section>
--- wp-content/themes/chicken/single.php
+++ wp-content/themes/chicken/single.php
@@ -9,7 +9,23 @@ switch (get_post_type()){
         break;
     case "nap":
         $acfData = get_field('nap-content',$post->ID );
-        get_template_part('front/single/nap', '', $acfData);
+        $terms = get_the_terms($post->ID, 'nap-tags');
+        $cats = [];
+        if(!is_wp_error($terms) and ($terms !== false)){
+            foreach ($terms as $termObj) {
+                array_push($cats, [
+                    'id' => $termObj->term_id,
+                    'name' => $termObj->name,
+                    'slug' => $termObj->slug,
+                    'count' => $termObj->count,
+                ]);
+            }
+        }
+        $data = [
+            'fields' => $acfData,
+            'cats' => $cats
+        ];
+        get_template_part('front/single/nap', '', $data);
         get_footer();
         break;
 }
