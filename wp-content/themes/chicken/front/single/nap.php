712e9ae5a4bbe2bd88eb65d9196702352f86ac3e
webazex <webazex@gmail.com>
2022-08-20 15:25:39 +0300

    somewhere

---
+++ wp-content/themes/chicken/front/single/nap.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<main>
+    <section>
+        <div class="site-size-nap">
+            <div class="site-size-nap__topped-row">
+                <div class="topped-row__breadcrumbs"></div>
+                <div class="topped-row__block-info"></div>
+            </div>
+
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
--- wp-content/themes/chicken/single.php
+++ wp-content/themes/chicken/single.php
@@ -1,10 +1,18 @@
 <?php
-wp_head();
+
 switch (get_post_type()){
     case "products":
+        wp_head();
         $dataProduct = get_field('product-group', $post->ID);
         get_template_part('front/single/product', '', $dataProduct);
+        wp_footer();
+        break;
+    case "nap":
+
+        $acfData = get_field('');
+        get_template_part('front/single/nap', '', $post);
+        get_footer();
         break;
 }
-wp_footer();
+
 ?>
