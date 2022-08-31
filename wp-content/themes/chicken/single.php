5715cca6cd444a2d2742c77d57a93fddeb5097ce
webazex <webazex@gmail.com>
2022-08-31 11:14:29 +0300

    add header and footer for product

--- wp-content/themes/chicken/single.php
+++ wp-content/themes/chicken/single.php
@@ -2,10 +2,10 @@
 
 switch (get_post_type()){
     case "products":
-        wp_head();
+        get_header();
         $dataProduct = get_field('product-group', $post->ID);
         get_template_part('front/single/product', '', $dataProduct);
-        wp_footer();
+        get_footer();
         break;
     case "nap":
         $acfData = get_field('nap-content',$post->ID );
