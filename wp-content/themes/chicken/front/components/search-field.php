feae745fc807fcc70340e53152f44fea5abe6558
phpeditor <phpeditor96@gmail.com>
2022-09-05 01:44:19 +0300

    Commit 9

--- wp-content/themes/chicken/front/components/search-field.php
+++ wp-content/themes/chicken/front/components/search-field.php
@@ -1,1 +1,1 @@
-<div class="header-container__search-box">    <input type="search" name="search" placeholder="<?php _e('Пошук', 'chicken'); ?>" class="s-input">    <div class="header-container__search-box-icon"></div></div>
+<div class="header-container__search-box">    <div class="header-container__search-box-icon"></div></div>
