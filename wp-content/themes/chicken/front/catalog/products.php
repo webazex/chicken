c3bf9169ba0973b24c111fa56b1ff8db406c9318
webazex <webazex@gmail.com>
2022-09-05 03:25:52 +0300

    fix merge errors

--- wp-content/themes/chicken/front/catalog/products.php
+++ wp-content/themes/chicken/front/catalog/products.php
@@ -35,39 +35,47 @@
                     <form class="default-grid-container__filters-row" method="post">
                         <div class="filters-row__group">
                             <?php
-                                if(!empty($args['subcats'])):
-                            ?>
-                            <div class="filters-row__filters">
-                                <div class="filter__text-row">
-                                    <span>Тип продукту</span>
-                                    <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M9.24944 0.164337C9.55557 0.44568 9.58585 0.974426 9.31451 1.30082L5.6921 5.65823C5.31328 6.11392 4.68672 6.11392 4.3079 5.65824L0.685489 1.30082C0.414154 0.974426 0.444427 0.44568 0.750559 0.164337C1.015 -0.0786886 1.39401 -0.0491219 1.62839 0.232816L4.3079 3.45601C4.68672 3.91169 5.31328 3.9117 5.6921 3.45601L8.3716 0.232817C8.60599 -0.0491214 8.985 -0.0786881 9.24944 0.164337Z" fill="#232323"/>
-                                    </svg>
+                            if(!empty($args['subcats'])):
+                                ?>
+                                <div id="filter-type" class="filters-row__filters">
+                                    <div class="filter__text-row">
+                                        <span>Тип продукту</span>
+                                        <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M9.24944 0.164337C9.55557 0.44568 9.58585 0.974426 9.31451 1.30082L5.6921 5.65823C5.31328 6.11392 4.68672 6.11392 4.3079 5.65824L0.685489 1.30082C0.414154 0.974426 0.444427 0.44568 0.750559 0.164337C1.015 -0.0786886 1.39401 -0.0491219 1.62839 0.232816L4.3079 3.45601C4.68672 3.91169 5.31328 3.9117 5.6921 3.45601L8.3716 0.232817C8.60599 -0.0491214 8.985 -0.0786881 9.24944 0.164337Z" fill="#232323"/>
+                                        </svg>
 
-                                </div>
-                                <div class="filters__filter-form cats" method="post">
-                                    <div class="cats-container">
-                                        <?php foreach ($args['subcats'] as $cat):?>
-                                        <label>
-                                            <input type="checkbox" name="term" value="<?php echo $cat['id'];?>">
-                                            <span><?php echo $cat['name']; ?></span>
-                                        </label>
-                                        <?php endforeach;?>
                                     </div>
-                                    <div class="btns-row">
-                                        <button type="submit"><?php _e('Застосувати', 'chicken'); ?></button>
-                                        <button type="reset"><?php _e('Очистити', 'chicken'); ?></button>
+                                    <div style="display: none" class="filter__text-row-select">
+                                        <span class="filter__text-row-select-name">Тип продукту: <span class="filter__text-row-select-counter"></span></span>
+                                        <div class="filter__text-row-select-clear"></div>
+                                    </div>
+                                    <div class="filters__filter-form cats" method="post">
+                                        <div class="cats-container">
+                                            <?php foreach ($args['subcats'] as $cat):?>
+                                                <label>
+                                                    <input type="checkbox" name="term" value="<?php echo $cat['id'];?>">
+                                                    <span><?php echo $cat['name']; ?></span>
+                                                </label>
+                                            <?php endforeach;?>
+                                        </div>
+                                        <div class="btns-row">
+                                            <button type="submit"><?php _e('Застосувати', 'chicken'); ?></button>
+                                            <button type="reset"><?php _e('Очистити', 'chicken'); ?></button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
                             <?php endif;?>
-                            <div class="filters-row__filters">
+                            <div id="filter-state" class="filters-row__filters">
                                 <div class="filter__text-row">
                                     <span><?php _e('Стан', 'chicken'); ?></span>
                                     <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M9.24944 0.164337C9.55557 0.44568 9.58585 0.974426 9.31451 1.30082L5.6921 5.65823C5.31328 6.11392 4.68672 6.11392 4.3079 5.65824L0.685489 1.30082C0.414154 0.974426 0.444427 0.44568 0.750559 0.164337C1.015 -0.0786886 1.39401 -0.0491219 1.62839 0.232816L4.3079 3.45601C4.68672 3.91169 5.31328 3.9117 5.6921 3.45601L8.3716 0.232817C8.60599 -0.0491214 8.985 -0.0786881 9.24944 0.164337Z" fill="#232323"/>
                                     </svg>
 
+                                </div>
+                                <div style="display: none" class="filter__text-row-select">
+                                    <span class="filter__text-row-select-name"><?php _e('Стан', 'chicken'); ?>: <span class="filter__text-row-select-counter"></span></span>
+                                    <div class="filter__text-row-select-clear"></div>
                                 </div>
                                 <div class="filters__filter-form">
                                     <label>
@@ -85,6 +93,7 @@
                                     </div>
                                 </div>
                             </div>
+                            <div style="display: none" class="filters-row__filters-clear"><div class="filters-row__filters-clear-icon"></div>Очистити фільтр</div>
                         </div>
 
                         <div class="filters-row__filters">
