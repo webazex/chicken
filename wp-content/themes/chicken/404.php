7b0f00313d48c544824a4afc44c67188ff4390ef
webazex <webazex@gmail.com>
2022-10-05 05:27:18 +0300

    404 1 of 2

--- wp-content/themes/chicken/404.php
+++ wp-content/themes/chicken/404.php
@@ -1,1 +1,17 @@
 <?php
+get_header();
+?>
+<main>
+    <section class="nf-main">
+        <div class="site-size">
+            <div class="site-size__content-nf">
+                <img src="" alt="">
+                <div class="content-nf__text-box">
+                    <h2><?php _e('Упс... 404', 'chicken');?></h2>
+                    <p><?php _e('ми запитали в кожної курочки, ало ніхто з них не знає куди подівалася ця сторінка', 'chicken');?></p>
+                    <a href="/" class="text-box__link-nf"><?php _e('До головної', 'chicken');?></a>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
